de21a120cee6d445494655bfb81e5da6265babfb
huolongshen <635360172@qq.com>
2022-10-26 13:15:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,7 +8,7 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  USE_PASSAGE: 'push-deer',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU17252TkWenO2sA7CDoyShXNrQ5UMoyhtzzlEM4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'LMM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
