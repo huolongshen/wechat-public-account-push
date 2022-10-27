5c1a9d32eb0785a6cbe785d962d411009dc7af5b
huolongshen <635360172@qq.com>
2022-10-27 21:31:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '自己',
+      name: '张狗1',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU17252TkWenO2sA7CDoyShXNrQ5UMoyhtzzlEM4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ZYL',
+      useTemplateId: 'ZYL1',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-14',
       festivals: [
@@ -48,14 +48,48 @@ const USER_CONFIG = {
       ],
     },
   ],
+  
+  
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '张狗2',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'PDU17252TkWenO2sA7CDoyShXNrQ5UMoyhtzzlEM4',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'ZYL2',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '04-14',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '1994', date: '04-14',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '*生日', name: '狗子2', year: '1994', date: '06-03',
+        },
+        {
+          type: '节日', name: '恋爱纪念日', year: '2022', date: '01-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-01-03' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+  ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'ZYL0',
+  CALLBACK_TEMPLATE_ID: 'ZYLback',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '查看返回消息',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU17252TkWenO2sA7CDoyShXNrQ5UMoyhtzzlEM4',
     }
